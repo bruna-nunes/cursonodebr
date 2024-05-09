5269262ab5f69c09dc3250f46ebc149a8d3946e6
bruna-nunes <brugnunes@gmail.com>
2024-05-08 21:04:06 -0300

    feat: add code from module 09 api delete

---
+++ module-09-7-hapi-delete/src/routes/heroesRoutes.js
@@ -0,0 +1,164 @@
+const BaseRoute = require('./base/baseRoute')
+const Joi = require('joi')
+const Boom = require('boom')
+
+class HeroesRoutes extends BaseRoute {
+    constructor(db) {
+        super()
+        this.db = db
+    }
+
+    list() {
+        return {
+            path: '/herois',
+            method: 'GET',
+            config: {
+                validate: {
+                    // payload -> body
+                    // headers => header
+                    // params -> na URL :id
+                    // query -> skip=10&limit=10
+                    failAction: (request, headers, erro) => {
+                        throw erro
+                    },
+                    query: {
+                        skip: Joi.number().integer().default(0),
+                        limit: Joi.number().integer().default(10),
+                        nome: Joi.string().min(3).max(200)
+                    }
+                }
+            },
+            handler: (request, headers) => {
+                try {
+                    const { skip, limit, nome } = request.query
+
+                    const query = {
+                        nome: {$regex: `.*${nome}*.`}
+                    }
+
+                    return this.db.read(
+                       nome ? query : {},
+                        skip,
+                        limit
+                    )
+                } catch(error) {
+                    console.log('Erro em list', error)
+                    return Boom.internal()
+                }
+                
+            }
+        }
+    }
+
+    create() {
+      return {
+        path: '/herois',
+        method: 'POST',
+        config: {
+            validate: {
+                failAction: (request, headers, erro) => {
+                    throw erro
+                },
+                payload: {
+                    nome: Joi.string().required().min(2).max(200),
+                    poder: Joi.string().required().min(2).max(100)
+                }
+            }
+        },
+        handler: async (request) => {
+            try {
+                const { nome, poder } = request.payload
+                const result = await this.db.create({nome, poder})
+                return {
+                    message: 'Heroi cadastrado com sucesso!',
+                    _id: result._id
+                }
+            } catch(error) {
+                console.log('Erro em create', error)
+                    return Boom.internal()
+            }
+        }
+      }  
+    }
+
+    update() {
+        return {
+            path: '/herois/{id}',
+            method: 'PATCH',
+            config: {
+                validate: {
+                    failAction: (request, headers, erro) => {
+                        throw erro
+                    },
+                    params: {
+                        id: Joi.string().required()
+                    },
+                    payload: {
+                        nome: Joi.string().min(2).max(200),
+                        poder: Joi.string().min(2).max(100)
+                    }
+                }
+            },
+            handler: async (request) => {
+                try {
+                    const { id } = request.params
+
+                    const { payload } = request
+
+                    const dadosString = JSON.stringify(payload)
+                    const dados = JSON.parse(dadosString)
+
+                    const result = await this.db.update(id, dados)
+
+                    if(result.modifiedCount !== 1) {
+                        return Boom.preconditionFailed('ID não encontrado no banco')
+                    }
+                    return {
+                        message: 'Heroi atualizado com sucesso!',
+                    }
+                } catch(error) {
+                    console.log('Erro em update', error)
+                    return Boom.internal()
+                }
+            }
+          } 
+    }
+
+    delete(){
+        return {
+            path: '/herois/{id}',
+            method: 'DELETE',
+            config: {
+                validate: {
+                    failAction: (request, headers, erro) => {
+                        throw erro
+                    },
+                    params: {
+                        id: Joi.string().required()
+                    },
+                }
+            },
+            handler: async (request) => {
+                try {
+                    const { id } = request.params
+
+                    const result = await this.db.delete(id)
+
+                    if(result.deletedCount !== 1) {
+                        return Boom.preconditionFailed('ID não encontrado no banco')
+
+                    }
+
+                    return {
+                        message: 'Heroi removido com sucesso!',
+                    }
+                } catch(error) {
+                    console.log('Erro em DELETE', error)
+                    return Boom.internal()
+                }
+            }
+          } 
+    }
+}
+
+module.exports = HeroesRoutes
---
+++ module-09-7-hapi-delete/src/tests/apiHeroes.test.js
@@ -0,0 +1,167 @@
+const assert = require('assert')
+const api = require('./../api')
+
+let app = {}
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Chapolin Colorado',
+    poder: 'Marreta biônica'
+}
+
+const MOCK_HEROI_INICIAL = {
+    nome: 'Gavião Negro',
+    poder: 'Flechas'
+}
+
+let MOCK_ID = ''
+
+describe('Suite de testes da API Heroes', function() {
+    this.beforeAll(async () => {
+        app = await api
+        const result = await app.inject({
+            method: 'POST',
+            url: '/herois',
+            payload: JSON.stringify(MOCK_HEROI_INICIAL)
+        })
+        const dados = JSON.parse(result.payload)
+
+        MOCK_ID = dados._id
+    })
+
+    it('listar /herois api', async () => {
+        const result = await app.inject({
+            method: 'GET',
+            url: '/herois?skip=0&limit=10'
+        })
+
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        
+        assert.deepEqual(statusCode, 200)
+        assert.ok(Array.isArray(dados))
+    })
+
+    it('listar /herois - deve retornar somente 10 registros', async () => {
+        const TAMANHO_LIMITE = 10
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        })
+
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        
+        assert.deepEqual(statusCode, 200)
+        assert.ok(dados.length === TAMANHO_LIMITE)
+    })
+
+    it('listar /herois - deve retornar um erro com limit incorreto', async () => {
+        const TAMANHO_LIMITE = 'stringaqui'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+        })
+        const errorResult = {
+            "statusCode": 400,
+            "error": "Bad Request",
+            "message": "child \"limit\" fails because [\"limit\" must be a number]",
+            "validation": {
+              "source": "query",
+              "keys": [
+                "limit"
+              ]
+            }
+          }
+        
+        assert.deepEqual(result.statusCode, 400)
+        assert.deepEqual(result.payload, JSON.stringify(errorResult))
+    })
+
+    it('listar /herois - deve filtrar um item', async () => {
+        const NAME = 'Homem aranha-1714437295556'
+
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=10&nome=${NAME}`
+        })
+
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        assert.deepEqual(statusCode, 200)
+        assert.ok(dados[0].nome === NAME)
+    })
+
+    it('cadastrar POST - /herois api', async () => {
+        const result = await app.inject({
+            method: 'POST',
+            url: `/herois`,
+            payload: JSON.stringify(MOCK_HEROI_CADASTRAR)
+        })
+
+        const statusCode = result.statusCode
+        const { message, _id } = JSON.parse(result.payload)
+
+        assert.ok(statusCode === 200)
+        assert.notStrictEqual(_id, undefined)
+        assert.deepEqual(message, 'Heroi cadastrado com sucesso!')
+
+    })
+    
+    it('atualizar PATCH - /herois/:id api', async () => {
+        const _id = MOCK_ID
+        const expected = {
+            poder: 'Super Mira'
+        }
+        const result = await app.inject({
+            method: 'PATCH',
+            url: `/herois/${_id}`,
+            payload: JSON.stringify(expected)
+        })
+
+        const statusCode = result.statusCode
+        const dados = JSON.parse(result.payload)
+
+        assert.ok(statusCode === 200)
+        assert.deepEqual(dados.message, 'Heroi atualizado com sucesso!')
+
+    })
+
+    it('atualizar PATCH - /herois/:id api - nao deve atualizar com ID incorreto', async () => {
+        const _id = '662c076cee038e6f767218b8'
+        const expected = {
+            poder: 'Super Mira'
+        }
+        const result = await app.inject({
+            method: 'PATCH',
+            url: `/herois/${_id}`,
+            payload: JSON.stringify(expected)
+        })
+
+        const statusCode = result.statusCode
+        const dados = JSON.parse(result.payload)
+        const expectedError = {
+            "statusCode": 412,
+            "error":"Precondition Failed",
+            "message":"ID não encontrado no banco"
+        }
+
+        assert.ok(statusCode === 412)
+        assert.deepEqual(dados, expectedError)
+
+    })
+
+    it('remover DELETE - /herois/:id', async () => {
+        const _id = MOCK_ID
+
+        const result = await app.inject({
+            method: 'DELETE',
+            url: `/herois/${_id}`,
+        })
+
+        const statusCode = result.statusCode
+        const dados = JSON.parse(result.payload)
+
+        assert.ok(statusCode === 200)
+        assert.deepEqual(dados.message, 'Heroi removido com sucesso!')
+        
+    })
+})
